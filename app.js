b7a70e086582eeaaa8d18d65b08a89a4fdb50fc9
manassetimoteo4 <manassetimoteo4@gmail.com>
2025-07-28 14:32:02 +0100

    feat: connecting to database and creating schemas

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ import { PORT } from "./configs/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-
+app.use(errorMiddleware);
 app.get("/", (req, res) =>
   res.send({ message: "Hello from the Subscriptions tracker server" })
 );
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`"Subscription tracker API running on http://localhost:${PORT}`);
+  await connectToDatabase();
 });
 
 export default app;
