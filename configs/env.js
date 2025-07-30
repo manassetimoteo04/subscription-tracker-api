2ca304bb34767a66c64b88b33577c99d8930b790
manassetimoteo4 <manassetimoteo4@gmail.com>
2025-07-30 11:42:34 +0100

    Adding Arcjet

--- configs/env.js
+++ configs/env.js
@@ -2,5 +2,12 @@
 import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, DB_URI, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const {
+  PORT,
+  DB_URI,
+  NODE_ENV,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_KEY,
+  ARCJET_ENV,
+} = process.env;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) =>
@@ -8,9 +12,7 @@ subscriptionRouter.get("/", (req, res) =>
 subscriptionRouter.get("/:id", (req, res) =>
   res.send({ title: "GET subscription details" })
 );
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE subscription" })
-);
+subscriptionRouter.post("/", authorize, createSubscription);
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "UPDADE subscription" })
 );
@@ -20,9 +22,8 @@ subscriptionRouter.delete("/:id", (req, res) =>
 subscriptionRouter.delete("/:id", (req, res) =>
   res.send({ title: "DELETE subscriptions" })
 );
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+
 subscriptionRouter.put("/:id/cancel", (req, res) =>
   res.send({ title: "CANCEL subscription" })
 );
